7c208ee7ad5a872c209dc46ae5427f0e00029971
Jack <lomax15@me.com>
2017-12-01 15:45:34 -0800

    made new gitignore, added some files, completed some to-dos

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,8 +1,8 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate  package.json and package-lock.json files. Don't forget to add node_modules to your .gitignore!
+// TODO DONE: Initialize your project using NPM to create and populate  package.json and package-lock.json files. Don't forget to add node_modules to your .gitignore!
 
-// TODO: Require the ExpressJS package that you installed via NPM, and instantiate the app.
+// TODO DONE: Require the ExpressJS package that you installed via NPM, and instantiate the app.
 // Remember to install ExpressJS, and be sure that it's been added to your package.json as a dependency.
 // There is also a package here called body-parser, which is used by the provided POST route. Be sure to install that and save it as a dependency after you create your package.json.
 
@@ -11,11 +11,13 @@ const PORT = 3000;
 
 // TODO: Include all of the static resources as an argument to app.use().
 
+app.use(express.static('./public'));
+
 // COMMENT: Why are our files in a "public" directory now? How does ExpressJS serve files?
-// PUT YOUR RESPONSE HERE
+// We are on the server side now so that we can make our files available for client requests. ExpressJS handles the requests from the clients and once it hears the request it runs the code that we provided. 
 
 // TODO: Refactor to use arrow function
-app.post('/articles', bodyParser, function(request, response) {
+app.post('/articles', bodyParser, (request, response) => {
     // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
     console.log(request.body);
     response.send('Record posted to server!!');
