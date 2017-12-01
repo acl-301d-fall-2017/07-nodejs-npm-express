d93f344bf23b08cd01c8d3437bd72efc562f2c18
Carmen Perezchica <crperezchica@gmail.com>
2017-12-01 15:10:36 -0800

    installed body-parser, instantiated expressJS and body-parse and refactored arrow function

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,21 +1,28 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate  package.json and package-lock.json files. Don't forget to add node_modules to your .gitignore!
+// Done - TODO: Initialize your project using NPM to create and populate  package.json and package-lock.json files. Don't forget to add node_modules to your .gitignore! 
 
-// TODO: Require the ExpressJS package that you installed via NPM, and instantiate the app.
+// Done -  TODO: Require the ExpressJS package that you installed via NPM, and instantiate the app.
+
+const express = require ('express');
+const app = express();
+const PORT = 4567;
+
+
 // Remember to install ExpressJS, and be sure that it's been added to your package.json as a dependency.
 // There is also a package here called body-parser, which is used by the provided POST route. Be sure to install that and save it as a dependency after you create your package.json.
 
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = 3000;
 
-// TODO: Include all of the static resources as an argument to app.use().
+//Done -  TODO: Include all of the static resources as an argument to app.use().
+app.use( express.static('./public'));
 
 // COMMENT: Why are our files in a "public" directory now? How does ExpressJS serve files?
-// PUT YOUR RESPONSE HERE
+// All of the files in the root "public" directory are the files that will run on the client side. This is where the server will pull/get files. ExpressJS serves the static files such as images, CSS and javascript files
 
 // TODO: Refactor to use arrow function
-app.post('/articles', bodyParser, function(request, response) {
+app.post('/articles', bodyParser, (request, response) => {
     // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
     console.log(request.body);
     response.send('Record posted to server!!');
