45d88ba74b617e3b78c77a95dd05f198a486665c
Charlie Heiner <heinercj@gmail.com>
2017-12-01 13:56:18 -0800

    added route to handle not definied requests by sending back 404 status

--- starter-code/server.js
+++ starter-code/server.js
@@ -11,12 +11,12 @@ const app = express();
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = 3000;
 
-// TODO: Include all of the static resources as an argument to app.use().
+// TODO DONE: Include all of the static resources as an argument to app.use().
 
+app.use(express.static('./public'));
 
 // COMMENT: Why are our files in a "public" directory now? How does ExpressJS serve files?
 // PUT YOUR RESPONSE HERE    We are now on the server side of the WRRC cycle so we add files into a 'public' directory that we want to make available to send to client requests.  We then make that folder availabe to use with the app.use line below.  ExpressJS serves files by handling requests from the client. It is similar to an event handler we would use on the front end, but in this case it listens for a certain request. Once it 'hears' that request it runs the code we want. 
-app.use(express.static('./public'));
 // TODO DONE: Refactor to use arrow function
 app.post('/articles', bodyParser, (request, response) => {
     // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
@@ -32,7 +32,7 @@ app.get('/new.html', (request, response) => {
 app.get('*',(request,response) => {
     response.status('404');
 });
-app.listen(PORT, function() {
+app.listen(PORT, () => {
     // TODO: Refactor this to arrow function, log to the console a message that lets you know which port your server has started on
-
+    console.log(`Port I'm using is ${PORT}` );
 });
