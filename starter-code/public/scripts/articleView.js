5a7b185ab9a7d587669dc46521b9b8bca5568a37
Carmen Perezchica <crperezchica@gmail.com>
2017-12-03 21:34:31 -0800

    finialized last questions

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -3,140 +3,140 @@
 const articleView = {};
 
 articleView.populateFilters = () => {
-  $('article').each(function() {
-    if (!$(this).hasClass('template')) {
-      let val = $(this).find('address a').text();
-      let optionTag = `<option value="${val}">${val}</option>`;
+    $('article').each(function() {
+        if (!$(this).hasClass('template')) {
+            let val = $(this).find('address a').text();
+            let optionTag = `<option value="${val}">${val}</option>`;
 
-      if ($(`#author-filter option[value="${val}"]`).length === 0) {
-        $('#author-filter').append(optionTag);
-      }
+            if ($(`#author-filter option[value="${val}"]`).length === 0) {
+                $('#author-filter').append(optionTag);
+            }
 
-      val = $(this).attr('data-category');
-      optionTag = `<option value="${val}">${val}</option>`;
-      if ($(`#category-filter option[value="${val}"]`).length === 0) {
-        $('#category-filter').append(optionTag);
-      }
-    }
-  });
+            val = $(this).attr('data-category');
+            optionTag = `<option value="${val}">${val}</option>`;
+            if ($(`#category-filter option[value="${val}"]`).length === 0) {
+                $('#category-filter').append(optionTag);
+            }
+        }
+    });
 };
 
 articleView.handleAuthorFilter = () => {
-  $('#author-filter').on('change', function() {
-    if ($(this).val()) {
-      $('article').hide();
-      $(`article[data-author="${$(this).val()}"]`).fadeIn();
-    } else {
-      $('article').fadeIn();
-      $('article.template').hide();
-    }
-    $('#category-filter').val('');
-  });
+    $('#author-filter').on('change', function() {
+        if ($(this).val()) {
+            $('article').hide();
+            $(`article[data-author="${$(this).val()}"]`).fadeIn();
+        } else {
+            $('article').fadeIn();
+            $('article.template').hide();
+        }
+        $('#category-filter').val('');
+    });
 };
 
 articleView.handleCategoryFilter = () => {
-  $('#category-filter').on('change', function() {
-    if ($(this).val()) {
-      $('article').hide();
-      $(`article[data-category="${$(this).val()}"]`).fadeIn();
-    } else {
-      $('article').fadeIn();
-      $('article.template').hide();
-    }
-    $('#author-filter').val('');
-  });
+    $('#category-filter').on('change', function() {
+        if ($(this).val()) {
+            $('article').hide();
+            $(`article[data-category="${$(this).val()}"]`).fadeIn();
+        } else {
+            $('article').fadeIn();
+            $('article.template').hide();
+        }
+        $('#author-filter').val('');
+    });
 };
 
 articleView.handleMainNav = () => {
-  $('.main-nav').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    $(`#${$(this).data('content')}`).fadeIn();
-  });
+    $('.main-nav').on('click', '.tab', function() {
+        $('.tab-content').hide();
+        $(`#${$(this).data('content')}`).fadeIn();
+    });
 
-  $('.main-nav .tab:first').click();
+    $('.main-nav .tab:first').click();
 };
 
 articleView.setTeasers = () => {
-  $('.article-body *:nth-of-type(n+2)').hide();
-  $('article').on('click', 'a.read-on', function(e) {
-    e.preventDefault();
-    if ($(this).text() === 'Read on →') {
-      $(this).parent().find('*').fadeIn();
-      $(this).html('Show Less &larr;');
-    } else {
-      $('body').animate({
-        scrollTop: ($(this).parent().offset().top)
-      },200);
-      $(this).html('Read on &rarr;');
-      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
-    }
-  });
+    $('.article-body *:nth-of-type(n+2)').hide();
+    $('article').on('click', 'a.read-on', function(e) {
+        e.preventDefault();
+        if ($(this).text() === 'Read on →') {
+            $(this).parent().find('*').fadeIn();
+            $(this).html('Show Less &larr;');
+        } else {
+            $('body').animate({
+                scrollTop: ($(this).parent().offset().top)
+            },200);
+            $(this).html('Read on &rarr;');
+            $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
+        }
+    });
 };
 
 // COMMENT: When/where is this function invoked? What event ultimately triggers its execution? Explain the sequence of code execution when this function is invoked.
-// PUT YOUR RESPONSE HERE
+// This function is invoked at the bottom of the new.html page. When the new.html loads. It shows the form and hides the preview initally, but when the user starts to type in content and tabs to a new section of the form the preview will populate.
 articleView.initNewArticlePage = () => {
-  $('.tab-content').show();
-  $('#export-field').hide();
-  $('#article-json').on('focus', function(){
-    this.select();
-  });
+    $('.tab-content').show();
+    $('#export-field').hide();
+    $('#article-json').on('focus', function(){
+        this.select();
+    });
 
-  $('#new-form').on('change', 'input, textarea', articleView.create);
-  $('#new-form').on('submit', articleView.submit);
+    $('#new-form').on('change', 'input, textarea', articleView.create);
+    $('#new-form').on('submit', articleView.submit);
 };
 
-// COMMENT: When is this function called? What event ultimately triggers its execution?
-// PUT YOUR RESPONSE HERE
+//Done- COMMENT: When is this function called? What event ultimately triggers its execution?
+// The function is called with in the articleView.initNewArticlePage function. Its triggered when their is a change in the input text area. 
 articleView.create = () => {
-  let article;
-  $('#articles').empty();
+    let article;
+    $('#articles').empty();
 
-  article = new Article({
-    title: $('#article-title').val(),
-    author: $('#article-author').val(),
-    authorUrl: $('#article-author-url').val(),
-    category: $('#article-category').val(),
-    body: $('#article-body').val(),
-    publishedOn: $('#article-published:checked').length ? new Date() : null
-  });
+    article = new Article({ //eslint-disable-line
+        title: $('#article-title').val(),
+        author: $('#article-author').val(),
+        authorUrl: $('#article-author-url').val(),
+        category: $('#article-category').val(),
+        body: $('#article-body').val(),
+        publishedOn: $('#article-published:checked').length ? new Date() : null
+    });
 
-  $('#articles').append(article.toHtml());
+    $('#articles').append(article.toHtml());
 
-  $('pre code').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
+    $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
 
-  $('#export-field').show();
-  $('#article-json').val(`${JSON.stringify(article)},`);
+    $('#export-field').show();
+    $('#article-json').val(`${JSON.stringify(article)},`);
 };
 
 // COMMENT: When is this function called? What event ultimately triggers its execution?
-// PUT YOUR RESPONSE HERE
+// This function is called within the articleView.initNewArticlePage. This is triggered when a user clicks submit. 
 articleView.submit = event => {
-  event.preventDefault();
-  let article = new Article({
-    title: $('#article-title').val(),
-    author: $('#article-author').val(),
-    authorUrl: $('#article-author-url').val(),
-    category: $('#article-category').val(),
-    body: $('#article-body').val(),
-    publishedOn: $('#article-published:checked').length ? new Date() : null
-  });
+    event.preventDefault();
+    const article = new Article({ //eslint-disable-line
+        title: $('#article-title').val(),
+        author: $('#article-author').val(),
+        authorUrl: $('#article-author-url').val(),
+        category: $('#article-category').val(),
+        body: $('#article-body').val(),
+        publishedOn: $('#article-published:checked').length ? new Date() : null
+    });
 
-  // COMMENT: Where is this function defined? When is this function called? What event ultimately triggers its execution?
-  // PUT YOUR RESPONSE HERE
-  article.insertRecord();
-}
+    // Done-  COMMENT: Where is this function defined? When is this function called? What event ultimately triggers its execution?
+    // This function defined in Article.prototype.insertRecord function that located in the article.js file. The function is called when a user enters data in the article form, but it's triggered when a user clicks submit. 
+    article.insertRecord();
+};
 
 articleView.initIndexPage = () => {
-  Article.all.forEach(article =>{
-    $('#articles').append(article.toHtml())
-  });
+    Article.all.forEach(article =>{ //eslint-disable-line
+        $('#articles').append(article.toHtml());
+    });
 
-  articleView.populateFilters();
-  articleView.handleCategoryFilter();
-  articleView.handleAuthorFilter();
-  articleView.handleMainNav();
-  articleView.setTeasers();
+    articleView.populateFilters();
+    articleView.handleCategoryFilter();
+    articleView.handleAuthorFilter();
+    articleView.handleMainNav();
+    articleView.setTeasers();
 };
