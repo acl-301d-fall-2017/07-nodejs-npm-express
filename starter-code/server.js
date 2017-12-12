4b0a4a1a6f80f4e634b626b6dd10134648e72511
Jack <lomax15@me.com>
2017-12-11 20:57:52 -0800

    refractored arrow function and wrote 2 routes using arrow functions

--- starter-code/server.js
+++ starter-code/server.js
@@ -9,14 +9,14 @@
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = 3000;
 
-// TODO: Include all of the static resources as an argument to app.use().
+// TODO DONE: Include all of the static resources as an argument to app.use().
 
 app.use(express.static('./public')); //eslint-disable-line
 
 // COMMENT: Why are our files in a "public" directory now? How does ExpressJS serve files?
 // We are on the server side now so that we can make our files available for client requests. ExpressJS handles the requests from the clients and once it hears the request it runs the code that we provided.
 
-// TODO: Refactor to use arrow function
+// TODO DONE: Refactor to use arrow function
 app.post('/articles', bodyParser, (request, response) => {
     // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
     console.log(request.body);
@@ -25,9 +25,19 @@ app.post('/articles', bodyParser, (request, response) => {
 
 // TODO: Write a new route, using an arrow function, that will handle a request and send the new.html file back to the user
 
+app.get('/new', (request, response) => {
+    response.sendFile('/public/new.html', {root:'.'});
+});
+
 // TODO: Write a new route, using an arrow function, that will handle any other routes that were not defined and deliver a 404 status message to the user
 
-app.listen(PORT, function() {
-    // TODO: Refactor this to arrow function, log to the console a message that lets you know which port your server has started on
+app.get('*', (request, response) => {
+    response.status('404').send({error:'Not found!'});
+});
+
+app.listen(PORT, () => {
+    // TODO DONE: Refactor this to arrow function, log to the console a message that lets you know which port your server has started on
+
+    console.log(PORT);
 
 });
